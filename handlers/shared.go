046d861acd32f880731d87587e58d8bb504d354a
rodrigo462003 <rodrigo46.2003@gmail.com>
2025-01-16 16:44:50 +0000

    Added initial navbar with sign in and register buttons.

--- handlers/shared.go
+++ handlers/shared.go
@@ -3,14 +3,13 @@ package handlers
 import (
 	"github.com/a-h/templ"
 	"github.com/labstack/echo/v4"
-	"github.com/rodrigo462003/FlickMeter/views/templates"
 )
 
 func Render(ctx echo.Context, statusCode int, t templ.Component) error {
 	buf := templ.GetBuffer()
 	defer templ.ReleaseBuffer(buf)
 
-	if err := templates.Base(t).Render(ctx.Request().Context(), buf); err != nil {
+	if err := t.Render(ctx.Request().Context(), buf); err != nil {
 		return err
 	}
 
