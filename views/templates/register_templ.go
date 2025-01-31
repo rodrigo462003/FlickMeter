31582fec9e1595f3cc26b4d180d8575a94c2d33f
rodrigo462003 <rodrigo46.2003@gmail.com>
2025-01-31 23:10:46 +0000

    Email Validation code form and other front-and styling issues.

--- views/templates/register_templ.go
+++ views/templates/register_templ.go
@@ -221,70 +221,30 @@ func FormVerifyCode(email string) templ.Component {
 			templ_7745c5c3_Var7 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 9, "<p class=\"text-center\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 9, "<div class=\"p-8 flex flex-col items-center justify-center space-y-10\"><div class=\"w-full\"><div class=\"w-full\"><div class=\"text-indigo-700 text-xl font-extrabold\">Enter your verification code</div><div class=\"md:text-sm text-xs justify-start\"><div class=\"inline-block whitespace-nowrap\"><pre>We sent a 6-digit code to </pre></div><div class=\"inline-block\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var8 string
 		templ_7745c5c3_Var8, templ_7745c5c3_Err = templ.JoinStringErrs(email)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/templates/register.templ`, Line: 35, Col: 11}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `views/templates/register.templ`, Line: 45, Col: 27}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var8))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 10, "</p>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 10, ".</div></div></div></div><div class=\"w-full flex flex-col items-center justify-center\"><form id=\"form\" class=\"w-full flex flex-col flex-grow text-indigo-700 space-y-4\"><div class=\"w-full max-w-full flex space-x-2 md:space-x-4\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Var9 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
-			templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
-			templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
-			if !templ_7745c5c3_IsBuffer {
-				defer func() {
-					templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
-					if templ_7745c5c3_Err == nil {
-						templ_7745c5c3_Err = templ_7745c5c3_BufErr
-					}
-				}()
-			}
-			ctx = templ.InitializeContext(ctx)
-			templ_7745c5c3_Var10 := templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
-				templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
-				templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templruntime.GetBuffer(templ_7745c5c3_W)
-				if !templ_7745c5c3_IsBuffer {
-					defer func() {
-						templ_7745c5c3_BufErr := templruntime.ReleaseBuffer(templ_7745c5c3_Buffer)
-						if templ_7745c5c3_Err == nil {
-							templ_7745c5c3_Err = templ_7745c5c3_BufErr
-						}
-					}()
-				}
-				ctx = templ.InitializeContext(ctx)
-				templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 11, "<div class=\"flex justify-center flex-row space-x-3\">")
-				if templ_7745c5c3_Err != nil {
-					return templ_7745c5c3_Err
-				}
-				for range 6 {
-					templ_7745c5c3_Err = digitInput().Render(ctx, templ_7745c5c3_Buffer)
-					if templ_7745c5c3_Err != nil {
-						return templ_7745c5c3_Err
-					}
-				}
-				templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 12, "<script>\n        function moveFocus(e, input) {\n            if (e.key !== \"Tab\") {\n                e.preventDefault()\n            }\n            if (e.key === \"Backspace\" || e.key === \"Delete\") {\n                if (input.value.length === 0) {\n                    let prevInput = input.previousElementSibling;\n                    if (prevInput && prevInput.tagName === \"INPUT\") {\n                        prevInput.focus();\n                    }\n                } else {\n                    input.value = \"\"\n                    let prevInput = input.previousElementSibling;\n                    if (prevInput && prevInput.tagName === \"INPUT\") {\n                        prevInput.focus();\n                    }\n                }\n                return\n            }\n            const isNumber = /^[0-9]$/i.test(event.key)\n            if (isNumber) {\n                if (input.value.length === 1) {\n                    let nextInput = input.nextElementSibling;\n                    if (nextInput && nextInput.tagName === \"INPUT\") {\n                        nextInput.focus();\n                        nextInput.value = e.key;\n                    }\n                } else {\n                    input.value = e.key\n                    let nextInput = input.nextElementSibling;\n                    if (nextInput && nextInput.tagName === \"INPUT\") {\n                        nextInput.focus();\n                    }\n                }\n            }\n        }\n    </script></div>")
-				if templ_7745c5c3_Err != nil {
-					return templ_7745c5c3_Err
-				}
-				return nil
-			})
-			templ_7745c5c3_Err = form().Render(templ.WithChildren(ctx, templ_7745c5c3_Var10), templ_7745c5c3_Buffer)
+		for range 6 {
+			templ_7745c5c3_Err = digitInput().Render(ctx, templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			return nil
-		})
-		templ_7745c5c3_Err = formContainer().Render(templ.WithChildren(ctx, templ_7745c5c3_Var9), templ_7745c5c3_Buffer)
+		}
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 11, "<script>\n                    function moveFocus(e, input) {\n                        if (e.key !== \"Tab\") {\n                            e.preventDefault()\n                        }\n                        if (e.key === \"Backspace\" || e.key === \"Delete\") {\n                            if (input.value.length === 0) {\n                                let prevInput = input.previousElementSibling;\n                                if (prevInput && prevInput.tagName === \"INPUT\") {\n                                    prevInput.focus();\n                                }\n                            } else {\n                                input.value = \"\"\n                                let prevInput = input.previousElementSibling;\n                                if (prevInput && prevInput.tagName === \"INPUT\") {\n                                    prevInput.focus();\n                                }\n                            }\n                            return\n                        }\n                        const isNumber = /^[0-9]$/i.test(event.key)\n                        if (isNumber) {\n                            if (input.value.length === 1) {\n                                let nextInput = input.nextElementSibling;\n                                if (nextInput && nextInput.tagName === \"INPUT\") {\n                                    nextInput.focus();\n                                    nextInput.value = e.key;\n                                }\n                            } else {\n                                input.value = e.key\n                                let nextInput = input.nextElementSibling;\n                                if (nextInput && nextInput.tagName === \"INPUT\") {\n                                    nextInput.focus();\n                                }\n                            }\n                        }\n                    }\n                </script></div></form></div></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -308,12 +268,12 @@ func digitInput() templ.Component {
 			}()
 		}
 		ctx = templ.InitializeContext(ctx)
-		templ_7745c5c3_Var11 := templ.GetChildren(ctx)
-		if templ_7745c5c3_Var11 == nil {
-			templ_7745c5c3_Var11 = templ.NopComponent
+		templ_7745c5c3_Var9 := templ.GetChildren(ctx)
+		if templ_7745c5c3_Var9 == nil {
+			templ_7745c5c3_Var9 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 13, "<input maxlength=\"1\" class=\"peer w-16 text-center placeholder:transition-opacity placeholder:duration-400 focus:placeholder:opacity-100 hover:placeholder:opacity-100\n            placeholder:opacity-0 text-gray-800 text-sm border border-indigo-700 rounded-md focus:outline-offset-0 focus:outline-none\n            focus:outline-indigo-700 p-2\" onkeydown=\"moveFocus(event, this)\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 12, "<input maxlength=\"1\" class=\"w-1/6 text-center placeholder:transition-opacity placeholder:duration-400 focus:placeholder:opacity-100 hover:placeholder:opacity-100\n            placeholder:opacity-0 text-gray-800 font-semibold border border-indigo-700 rounded-md focus:outline-offset-0 focus:outline-none\n            focus:outline-indigo-700 p-2\" onkeydown=\"moveFocus(event, this)\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
